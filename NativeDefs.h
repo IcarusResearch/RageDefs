8bdbff9d236eb1a8c8fb934c2e56de2acce7aac4
Deniz Groenhoff <deniz.groenhoff@uni-oldenburg.de>
2023-04-10 23:00:50 +0200

    feat(Natives): Added some definitions related to the native context

---
+++ NativeDefs.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Windows.h>
+
+namespace Rage {
+
+	typedef struct Vec3F {
+		alignas(0x08) FLOAT x;
+		alignas(0x08) FLOAT y;
+		alignas(0x08) FLOAT z;
+	} VecF, *PVecF;
+
+	typedef struct alignas(0x10) Vec3FA {
+		FLOAT x;
+		FLOAT y;
+		FLOAT z;
+	} Vec3FA, *PVec3FA;
+
+	typedef struct NativeCallContext {
+		PVOID pReturnValue;					// 0x00
+		UINT32 uParamCount;					// 0x08
+		PVOID pParams;						// 0x10
+		UINT32 uDataCount;					// 0x18
+		PVecF vecsOut[0x04];				// 0x20
+		Vec3FA vecsIn[0x04];				// 0x40
+		BYTE padding[0x60];					// 0x80
+	} NativeCallContext, *PNativeCallContext;
+
+	typedef INT64 NATIVEHASH;
+
+	using NativeHandler = VOID(*)(PNativeCallContext);
+	using GetNativeHandler = NativeHandler(*)(PVOID, NATIVEHASH);
+	using FixVectors = VOID(*)(PNativeCallContext);
+
+}
