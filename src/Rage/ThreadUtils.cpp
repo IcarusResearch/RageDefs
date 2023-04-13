894e87ae2b7d2422009b94997590f295e9bdbb07
Deniz Groenhoff <deniz.groenhoff@uni-oldenburg.de>
2023-04-13 22:07:07 +0200

    fix(ThreadUtils): Fixed compiler error

--- src/Rage/ThreadUtils.cpp
+++ src/Rage/ThreadUtils.cpp
@@ -2,11 +2,11 @@
 
 #include <intrin.h>
 
-PTLSContext Rage::GetCurrentTLSContext() {
+Rage::PTLSContext Rage::GetCurrentTLSContext() {
 	return *reinterpret_cast<TLSContext**>(__readgsqword(0x58));
 }
 
-PThread Rage::FindScriptThread(JOAAT threadHash, PArray<PThread> pThreads) {
+Rage::PThread Rage::FindScriptThread(JOAAT threadHash, PArray<PThread> pThreads) {
 	for (PThread pThread : *pThreads) {
 		if (pThread && pThread->threadContext.uThreadId && pThread->threadContext.uScriptHash == threadHash) {
 			return pThread;
